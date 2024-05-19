e176b29374c7a956b5d730f32be447c31bdd05be
Potatomastr27 <nathan.r.turner@student.uts.edu.au>
2024-05-19 16:22:45 +1000

    Added new tests folder for testing code

---
+++ src/tests/pcbTest.cpp
@@ -0,0 +1,30 @@
+#include <Arduino.h>
+#include "../pinDef.h"
+#include "../lcd.h"
+
+long prevBlink = 0;
+
+
+void setup() {
+    Serial.begin(9600);
+
+    setupPins();
+    setupLCD();
+    
+    Serial.println("Begin");
+    delay(10000);
+}
+
+void loop() {
+
+    // Blink all LEDS every 500ms seperated by 100ms only when the button is released
+    if (millis() - prevBlink > 500 && digitalRead(SWITCH_SUBROUTINES_PIN == BUTTON_RELEASED)){
+        digitalWrite(STATIONARY_PIN, !digitalRead(STATIONARY_PIN)); // Toggle Pin
+        delay(100);
+        digitalWrite(WALKING_PIN, !digitalRead(WALKING_PIN)); // Toggle Pin
+        delay(100);
+        digitalWrite(RUNNING_PIN, !digitalRead(RUNNING_PIN)); // Toggle Pin
+        delay(100);
+        prevBlink = millis();
+    }
+}
