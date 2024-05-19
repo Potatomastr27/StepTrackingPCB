e1675f7f4f5f98acbbbd9681dccd6cbcc0328b35
Potatomastr27 <nathan.r.turner@student.uts.edu.au>
2024-05-19 18:19:35 +1000

    Added Serial.prints to displayTest.cpp

--- src/tests/displayTest.cpp
+++ src/tests/displayTest.cpp
@@ -9,17 +9,24 @@ void setup() {
     Serial.begin(9600);
 
     setupPins();
+    Serial.println("Setting up LCD");
     setupLCD();
     
     Serial.println("Begin");
-
+    Serial.println("Sending Hello World to LCD");
     printLCD("Hello World\r\nHelloWorld2\r\n012345678901234567890123456789");
+    Serial.println("Done, waiting 10 seconds");
     delay(10000);
 }
 
 void loop() {
     for(int i = 0; i < 1000; i++){
+        Serial.println("Using sprintf");
         sprintf(lcdBuffer, "Heres a number: %i", i);
+        // Debug code
+        Serial.print("Buffer Contents: ");
+        Serial.println(lcdBuffer);
+        Serial.println("Sending to LCD");
         printLCD(0,0, lcdBuffer,2,WHITE);
     }
 }
