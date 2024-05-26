3228f1a7a6236aa8d8963e695849dcbdf8d38259
Potatomastr27 <nathan.r.turner@student.uts.edu.au>
2024-05-26 16:52:03 +1000

    Fixed PaceIdentification

---
+++ lib/PaceIdentification/PaceIdentification.h
@@ -0,0 +1,14 @@
+#ifndef PACEIDENTIFICATION_H
+#define PACEIDENTIFICATION_H
+
+#include <Arduino.h>
+#include "../../include/pinDef.h" //!!! This include was wrong
+
+//!!! Files were in the wrong Directory
+
+void updatePace(int movementRate);
+
+// Add the function declaration here
+/* void shutdownError(const char* msg); */ //!!! This is not defined so the code errors
+
+#endif
